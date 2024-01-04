72603ad44d07c2cb3f9a11f3518112f5ddf08227
Ignacio <ignaciogimenez70@gmail.com>
2024-01-04 19:58:32 -0300

    homework 01 finished

--- 01-Node/homework/01 - Exercises/bash.js
+++ 01-Node/homework/01 - Exercises/bash.js
@@ -2,7 +2,27 @@ const process = require('process');
 const { Z_ASCII } = require('zlib');
 const commands = require('./commands/index.js');
 
-function bash() {}
+function bash() {
+   process.stdout.write('prompt > ')
+
+   process.stdin.on("data", (data) => {
+      let args = data.toString().trim().split(' ') // [npm, install]
+
+      let cmd = args.shift()//args[0] // npm
+      // arg = args.slice(1)
+
+      if (!commands[cmd]) {
+         print(`command not found: ${cmd}`)
+      } else {
+         commands[cmd](print, args.join(' '))
+      }
+   })
+}
+
+function print(output) {
+   process.stdout.write(output)
+   process.stdout.write('\nprompt > ')
+}
 
 bash();
 module.exports = {
--- 01-Node/homework/01 - Exercises/commands/index.js
+++ 01-Node/homework/01 - Exercises/commands/index.js
@@ -1,21 +1,76 @@
 const fs = require("fs");
 const utils = require("../utils/request");
 const process = require("process");
+const { throws } = require("assert");
 
-function pwd() {}
+function pwd(print) {
+    print(process.cwd())
+}
 
-function date() {}
+function date(print) {
+    // print(typeof new Date()) object
+    // print(typeof Date()) string
+    print(Date())
+}
 
-function echo() {}
+function echo(print, args) {
+    print(args)
+}
 
-function ls() {}
+function ls(print) {
+    fs.readdir(".", (err, files) => {
+        if(err) throw Error(err)
+        print(files.join(' '))
+    })
+}
 
-function cat() {}
+function cat(print, args) {
+    fs.readFile(args, 'utf-8', (err, data) => {
+        if (err) throw Error(err)
+        print(data)
+    })
+}
 
-function head() {}
+function head(print, args) {
+    fs.readFile(args, 'utf-8', (err, data) => {
+        if (err) throw Error(err)
+        // let lines = data.split("\n").slice(0, 8).join('\n') 
+        let lines = data.split("\n")[0] // Solo imprimir la primer linea
+        print(lines)
+    })
+}
 
-function tail() {}
+function tail(print, args) {
+    fs.readFile(args, 'utf-8', (err, data) => {
+        if (err) throw Error(err)
+        let line = data.split("\n").at(-1).trim() // [-1]
+        // let lastLine = line[line.length - 1]
+        print(line)
+    })
+}
 
-function curl() {}
+function curl(print, args) { // https://google.com
+    utils.request(args, (err, response) => {
+        if (err) throw Error(err)
+        print(response)
+    })
+}
 
-module.exports = {};
+function curl2(print, args) { // google.com
+    utils.request(`https://${args}`, (err, response) => {
+        if(err) throw Error(err)
+        print(response) 
+    })
+}
+
+module.exports = {
+    pwd,
+    date,
+    echo,
+    ls,
+    cat,
+    head,
+    tail,
+    curl,
+    curl2
+};
